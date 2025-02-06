fd2d4420137bfab1edaf06152dcf98d5278f1c57
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-02-06 22:12:39 +0100

    feat: fix ex

--- src/components/AccordionList.jsx
+++ src/components/AccordionList.jsx
@@ -3,7 +3,7 @@ import Accordion from "./Accordion";
 
 export default function AccordianList() {
     return (
-        <div className="accordion-list">
+        <div>
             {languages.map((language) => (
                 <Accordion key={language.id} title={language.title} description={language.description} />
             ))}
